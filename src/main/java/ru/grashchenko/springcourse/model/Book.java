7d0b8cd581718f5089207c01a99e062b6056a6c8
Vladi207 <graschenko207@mail.ru>
2022-09-17 14:31:02 +0300

    refactor model

--- src/main/java/ru/grashchenko/springcourse/model/Book.java
+++ src/main/java/ru/grashchenko/springcourse/model/Book.java
@@ -1,20 +1,29 @@
 package ru.grashchenko.springcourse.model;
 
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "title")
     @NotEmpty(message = "Название книги не должно быть пустым")
     @Size(min = 2, max = 100, message = "Название книги должно быть от 2 до 100 символов длиной")
     private String title;
 
+    @Column(name = "author")
     @NotEmpty(message = "Автор не должен быть пустым")
     @Size(min = 2, max = 100, message = "Имя автора должно быть от 2 до 100 символов длиной")
     private String author;
 
+    @Column(name = "year")
     @Min(value = 1500, message = "Год должен быть больше, чем 1500")
     private int year;
 
--- src/main/java/ru/grashchenko/springcourse/model/Person.java
+++ src/main/java/ru/grashchenko/springcourse/model/Person.java
@@ -1,16 +1,24 @@
 package ru.grashchenko.springcourse.model;
 
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "full_name")
     @NotEmpty(message = "Имя не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
     private String fullName;
 
+    @Column(name = "year of_birth")
     @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
     private int yearOfBirth;
 
